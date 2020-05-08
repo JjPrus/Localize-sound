ac2e3c475001c0609c840d9b1c5018a2a750c1ec
sonofsabbath <sonofsabbath@users.noreply.github.com>
2020-05-08 16:02:20 +0200

    rewrote solver to scipy

--- Wykrywanie 30s.py
+++ Wykrywanie 30s.py
@@ -1,14 +1,11 @@
 from scipy.io.wavfile import read
 from scipy import signal
+from scipy.optimize import root
+from numpy.random import uniform
 import numpy as np
 import matplotlib.pyplot as plt
 import pyaudio
 import wave
-from sympy import Symbol, nsolve, Rational
-import sympy
-import mpmath
-from random import randrange
-
 
 
 def lag_finder(y1, y2, sr):
@@ -23,7 +20,7 @@ def lag_finder(y1, y2, sr):
 
     delay_arr = np.linspace(-0.5*n/sr, 0.5*n/sr, n)
     delay = delay_arr[np.argmax(corr)]
-    return delay/100
+    return delay
 
 
 def get_audio(chunk, channels, rate, record_time):
@@ -63,67 +60,48 @@ def get_audio(chunk, channels, rate, record_time):
     wf.close()
 
 
-def solve_equation(x,y,d):
-    # Równanie powiędzy 1 i 2
-    f1 = sympy.sqrt(np.abs((x[1]-a)**2-(y[1]-b)**2)) - sympy.sqrt(np.abs((x[0]-a)**2-(y[0]-b)**2)) - d[0]
-    # Równanie powiędzy 1 i 3
-    f2 = sympy.sqrt(np.abs((x[2]-a)**2-(y[2]-b)**2)) - sympy.sqrt(np.abs((x[0]-a)**2-(y[0]-b)**2)) - d[1]
-    # Równanie powiędzy 2 i 3
-    f3 = sympy.sqrt(np.abs((x[2]-a)**2-(y[2]-b)**2)) - sympy.sqrt(np.abs((x[1]-a)**2-(y[1]-b)**2)) - d[2]
-    x_dz = []
-    y_dz = []
-    while len(x_dz) < 3:
-        pp1 = np.random.uniform(x[0]-5, x[2] + 5)
-        pp2 = np.random.uniform(y[0]-5, y[2] + 5)
-
-        try:
-            p,q = nsolve((f1,f2,f3), (a,b), (pp1,pp2),verify=False)
-            if abs(p) < 10 and (0 < q < 10):
-                x_dz.append(p)
-                y_dz.append(q)
-        except:
-            pass
-    # print(x_dz)
-    # print(y_dz)
-
-    return np.median(x_dz), np.median(y_dz)
+def fun(x):
+    return [np.sqrt(np.abs((wx[1] - x[0]) ** 2 - (wy[1] - x[1]) ** 2)) - np.sqrt(np.abs((wx[0] - x[0]) ** 2
+            - (wy[0] - x[1]) ** 2)) - d[0], np.sqrt(np.abs((wx[2] - x[0]) ** 2 - (wy[2] - x[1]) ** 2))
+            - np.sqrt(np.abs((wx[0] - x[0]) ** 2 - (wy[0] - x[1]) ** 2)) - d[1]]
 
 
-
-
-mpmath.mp.dps = 8
 # Współrzędne mikrofonu
-x = [-0.30,0,0.55]
-y = [0,0,0]
-
-# Współrzędne głośnika
-a = Symbol('a', real=True)
-b = Symbol('b', real=True)
-
-
+wx = [-0.40,0,0.50]
+wy = [1,1,1]
 
 sample_rate, data = read('dlugie.wav')
-t = np.arange(len(data))
+'''t = np.arange(len(data))
 plt.plot(t,data[:,0:1])
 plt.show()
 plt.plot(t,data[:,2:3])
 plt.show()
 plt.plot(t,data[:,4:5])
-plt.show()
+plt.show()'''
 
 for i in range(0,int(len(data)/sample_rate)):
     data1 = data[i*sample_rate : (i+1)*sample_rate, :1]
     data2 = data[i*sample_rate : (i+1)*sample_rate, 2:3]
     data3 = data[i*sample_rate : (i+1)*sample_rate, 4:5]
 
-    d = [lag_finder(data1, data2, data1.shape[0]), lag_finder(data1, data3, data1.shape[0]), lag_finder(data2, data3, data1.shape[0])]
-    # print(d)
+    d = [lag_finder(data1, data2, data1.shape[0]), lag_finder(data1, data3, data1.shape[0]),
+         lag_finder(data2, data3, data1.shape[0])]
 
-    x_dz, y_dz = solve_equation(x,y,d)
-    # print(i, x_dz , y_dz)
-    # plt.xlim(-0.5,0.7)
-    # plt.ylim(-0.1,0.7)
+    a = []
+    b = []
+    while len(a) < 10 and len(b) < 10:
+        sol = root(fun, np.array([uniform(-1, 1), uniform(-1, 1)]))
+        ai, bi = sol.x
+        if -5 < ai < 5 and -1 < bi < 1:
+            a.append(ai)
+            b.append(bi)
+
+    print(a, b)
+    aa = np.mean(a)
+    bb = np.mean(b)
+    print(aa, bb)
+
     plt.title(i)
-    plt.scatter(x,y)
-    plt.scatter(x_dz,y_dz)
+    plt.scatter(wx,wy)
+    plt.scatter(aa,bb)
     plt.show()
